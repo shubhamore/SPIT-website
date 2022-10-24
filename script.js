728ed47d9cb43cf0a721319f6272abd506d8b5ac
Shubham <shubhamore02@gmail.com>
2022-10-24 17:58:21 +0530

    completed about and footer section . also updated images  and videos

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ function showSlides() {
     slides[slideindex - 1].style.display = "block"
     setTimeout(showSlides, 3000)
 }
+
+
+//for animating slide
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
