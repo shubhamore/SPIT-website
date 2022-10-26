8bf7b7435cb87e65e1964ee61987499efee4b6fc
Shubham <shubhamore02@gmail.com>
2022-10-26 23:31:35 +0530

    added alumni and cta section along with few photos

--- script.js
+++ script.js
@@ -105,3 +105,46 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
+//for alumni cards
+// let alumni=document.querySelector(".alumni")
+// let listOfBackground=["linear-gradient(to right, #5614b0, #dbd65c)","linear-gradient(to right, #c2e59c, #64b3f4)"]
+let bgIndex=0
+let cardToAppear=0
+let startCardIndex=0
+function findwidth(){
+    if(window.screen.width>850){
+        cardToAppear=3
+    }
+    else if(window.screen.width>575){
+        cardToAppear=2
+    }
+    else{
+        cardToAppear=1
+    }
+}
+function bringCards(){
+    // changeBackground()
+    let order=1
+    let alumniCards=document.querySelectorAll(".alumni-card")
+    for(let i=0;i<alumniCards.length;i++){
+        alumniCards[i].classList.add("no")
+        alumniCards[i].style.order=null
+    }
+    for(let i=0;i<cardToAppear;i++){
+        let toRemove=(startCardIndex+i)%alumniCards.length
+        alumniCards[toRemove].style.order=order
+        alumniCards[toRemove].classList.remove("no")
+        order++
+    }
+    startCardIndex=(startCardIndex+1)%alumniCards.length
+}
+// function changeBackground(){
+//     console.log("bgIndex="+bgIndex);
+//     console.log("listOfBackground[bgIndex]="+listOfBackground[bgIndex]);
+//     console.log("listOfBackground.length="+listOfBackground.length);
+//     document.querySelector(".alumni").style.background=`${listOfBackground[bgIndex]}`
+//     bgIndex=(bgIndex+1)%listOfBackground.length
+// }
+bringCards()
+setInterval(findwidth,1000)
+setInterval(bringCards,3000)
